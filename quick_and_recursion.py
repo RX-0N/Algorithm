29b29fff2acefe6fb21bd7157aa32636187553a4
Asinus <burglar832@gmail.com>
2024-02-15 09:43:39 +0900

    최대값 찾기 함수 코드 오류 수정
    Signed-off-by: Frankenstein <burglar832@gmail.com>

--- quick_and_recursion.py
+++ quick_and_recursion.py
@@ -135,7 +135,7 @@ def max(list):
     if len(list) == 2:
         return list[0] if list[0] > list[1] else list[1]
     sub_max = max(list[1:])
-    return list[0] if list[0] > sub_max else list[1]
+    return list[0] if list[0] > sub_max else sub_max
 
 temp = 0
 random.shuffle(testArr)
