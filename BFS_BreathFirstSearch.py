ccdd4b24a841f14c0ac283e1bcfc35f0227b5f50
Asinus <burglar832@gmail.com>
2024-02-27 09:34:22 +0900

    search 함수를 사용할 수 있는BFS로 만들어보기
    Signed-off-by: Frankenstein <burglar832@gmail.com>

--- BFS_BreathFirstSearch.py
+++ BFS_BreathFirstSearch.py
@@ -2,9 +2,30 @@ from collections import deque
 
 graph = {}
 
-def person_is_seller(person):
-    print("is seller?")
+# asinus = {'아시누스':False}
+# teemo = {'티모': True}
+# nanus = {'나서스': False}
+# ezreal = {'나서스': False}
+# vayne = {'베인':False}
+# jhin = {'진':False}
+#graph["asinus"] = {teemo, nanus, ezreal, vayne, jhin}
 
+graph['아시누스'] = {'티모': True, '나서스': False, '나서스': False, '베인':False, '진':False}
+print(graph['아시누스'].get('티모'))
+
+# 각 인원별로 자신과 연결된 정점 목록을 생성
+
+def who_is_seller(person):
+    print("is seller?")
+    target = {}
+    target = person
+
+    for seller in target:
+        if seller.values():
+            return True
+    return False
+
+#누가 버섯을 파는가?
 def search(name):
     search_queue = deque()
     search_queue += graph[name]
@@ -13,12 +34,12 @@ def search(name):
     while search_queue:
         person = search_queue.popleft()
         if not person in searched:
-            if person_is_seller(person):
-                print person + " is a margo seller!"
+            if who_is_seller(person):
+                print(person + " is a mushroom seller!")
                 return True
             else:
                 search_queue += graph[person]
                 searched.append(person)
     return False
 
-search("you")
+search()
